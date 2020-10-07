0665827f19ea5c54b5c43f90dbd78c018650b6c8
Serin <serin0837@yahoo.com>
2020-10-07 18:14:49 +0100

    use react-bootstrap dropdown

--- backlog.md
+++ backlog.md
@@ -4,5 +4,11 @@
   good article about react-router-dom https://codingbroker.tistory.com/72
 
 - 2020.10.06
+
   - how to create imaginary-country different data place? and how can i access region and access depends on region
   - functions to count how many country been
+
+    2020.10.07
+
+- figure out how to render depends on region?? uhm.. add dropdown but do i need to make each component? or..
+- npm react-bootstrap
--- src/component/CountryList.jsx
+++ src/component/CountryList.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import axios from "axios";
 import Loader from "./Loader";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 
 class CountryList extends Component {
   state = {
     countries: [],
     isLoading: true,
+    value: "",
   };
 
   componentDidMount() {
@@ -19,28 +23,56 @@ class CountryList extends Component {
   render() {
     const { isLoading, countries } = this.state;
     if (isLoading) return <Loader />;
+    const handleSelect = (event) => {
+      console.log(event);
+    };
     return (
-      <main className="country">
-        {countries.map((country) => {
-          return (
-            <ul key={country._id}>
-              <li className="countryTitle">{country.name}</li>
-              <li>
-                <img src={country.flag} alt={country.name}></img>
-              </li>
-              <li>
-                <strong>Capital:</strong> {country.capital}
-              </li>
-              <li>
-                <strong>Region:</strong> {country.region}
-              </li>
-              <li>
-                <strong>Population:</strong> {country.population}
-              </li>
-            </ul>
-          );
-        })}
-      </main>
+      <div>
+        <DropdownButton
+          alignRight
+          title="Country by Region"
+          id="dropdown-menu-align-right"
+          onSelect={handleSelect}
+        >
+          <Dropdown.Item eventKey="Africa" href="/region/Africa">
+            Africa
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="Americas" href="/region/America">
+            Americas
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="Asia" href="/region/Asia">
+            Asia
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="Europe" href="/region/Europe">
+            Europe
+          </Dropdown.Item>
+          <Dropdown.Item eventKey="Oceania" href="/region/Oceania">
+            Oceania
+          </Dropdown.Item>
+        </DropdownButton>
+        <Route path="/region/:value" />
+        <main className="country">
+          {countries.map((country) => {
+            return (
+              <ul key={country._id}>
+                <li className="countryTitle">{country.name}</li>
+                <li>
+                  <img src={country.flag} alt={country.name}></img>
+                </li>
+                <li>
+                  <strong>Capital:</strong> {country.capital}
+                </li>
+                <li>
+                  <strong>Region:</strong> {country.region}
+                </li>
+                <li>
+                  <strong>Population:</strong> {country.population}
+                </li>
+              </ul>
+            );
+          })}
+        </main>
+      </div>
     );
   }
 }
